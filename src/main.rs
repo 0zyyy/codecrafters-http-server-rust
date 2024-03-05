384fc2df13cee50cf0411b14d3be8282c7043ec7
Jalu Fazlurrahman <77714640+0zyyy@users.noreply.github.com>
2024-03-05 07:48:28 +0700

    (feat): added http response

--- src/main.rs
+++ src/main.rs
@@ -33,9 +33,86 @@ fn handle_client(mut stream: TcpStream){
     let path = lines[0].split(" ").collect::<Vec<&str>>()[1];
 
     let resp = match path {
-        "/" => "HTTP/1.1 200 OK\r\n\r\n",
-        _ =>"HTTP/1.1 404 Not Found\r\n\r\n",
+        "/" => HttpResponse {
+            status_code: HttpResponseCode::Ok,
+            ..HttpResponse::default()
+        },
+        _ => if path.starts_with("/echo/") {
+            let (_,echo) = path.split_once("/echo/").unwrap();
+            HttpResponse {
+                status_code: HttpResponseCode::Ok,
+                content_type: String::from("text/plain"),
+                content_length: echo.len() as i32,
+                content: echo.to_string(),
+            }
+        } else {
+            HttpResponse{
+                status_code: HttpResponseCode::NotFound,
+                ..HttpResponse::default()
+            }
+        },
     };
-    stream.write_all(resp.as_bytes()).unwrap()
+    stream.write_all(resp.to_string().as_bytes()).unwrap()
 
+}
+
+enum HttpResponseCode {
+    Ok,
+    NotFound
+}
+
+impl HttpResponseCode {
+    fn code(&self) -> i32 {
+        match self {
+            HttpResponseCode::Ok => 200,
+            HttpResponseCode::NotFound => 404,
+        }
+    }
+
+    fn text(&self) -> &str {
+        match self {
+            HttpResponseCode::Ok => "",
+            HttpResponseCode::NotFound => "",
+        }
+    }
+}
+
+struct HttpResponse {
+    status_code: HttpResponseCode,
+    content_type: String,
+    content_length: i32,
+    content: String
+}
+
+impl Default for HttpResponse {
+    fn default() -> Self {
+        Self {
+            status_code: HttpResponseCode::Ok,
+            content_type: String::new(),
+            content_length: 0,
+            content: String::new()
+        }
+    }
+}
+
+impl HttpResponse {
+    fn to_string(&self) -> String {
+        let mut resp: String = String::from("HTTP/1.1 ");
+        resp.push_str(self.status_code.code().to_string().as_str());
+        resp.push_str(" ");
+        resp.push_str(self.status_code.text());
+        resp.push_str("\r\n");
+        if !self.content.is_empty() {
+            resp.push_str("Content-Type: ");
+            resp.push_str(self.content_type.as_str());
+            resp.push_str("\r\n");
+            resp.push_str("Content-Length: ");
+            resp.push_str(self.content_length.to_string().as_str());
+            resp.push_str("\r\n\r\n");
+            resp.push_str(self.content.as_str());
+            resp.push_str("\r\n");
+        }
+        resp.push_str("\r\n");
+        resp
+    }
 }
